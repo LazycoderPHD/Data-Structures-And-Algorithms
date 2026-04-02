d7508b60a0a1f13fa1f8f33fce3053fe0d67b573
LazycoderPHD <phatdao2309@gmail.com>
2026-04-02 14:10:47 +0700

    WIP

---
+++ queue.cs
@@ -0,0 +1,16 @@
+namespace DataStructures;
+
+public class QueueDemo
+{
+    public static void RunQueueDemo()
+    {
+        // Queue = FIFO data structure. First-In First-Out
+		//		   A collection designed for holding elements prior to processing
+		//		   Linear data structure
+		
+		//        Add = Enqueue()
+		//        rm = Dequeue()
+
+        
+    }
+}
