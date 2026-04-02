ade775692168cff4fdf967f8926334c9a73ce85b
LazycoderPHD <phatdao2309@gmail.com>
2026-04-02 14:39:04 +0700

    queue completed

--- queue.cs
+++ queue.cs
@@ -18,9 +18,22 @@ public class QueueDemo
         queue.Enqueue("Steve");
         queue.Enqueue("Drake");
 
+        queue.Dequeue(); // will cause an exception if you dequeue more than you have on enqueue
+        queue.Dequeue();
+        queue.Dequeue();
+
+
         System.Console.WriteLine(string.Join(", ", queue));
+        // System.Console.WriteLine(queue.Peek());
+        System.Console.WriteLine(queue.Count);
+        System.Console.WriteLine(queue.Contains("Karen"));
+        System.Console.WriteLine(queue.Contains("Drake"));
 
-
-        
+        // Where are queues useful?
+		
+		// 1. Keyboard Buffer (letters should appear on the screen in the order they're pressed)
+		// 2. Printer Queue (Print jobs should be completed in order)
+		// 3. Used in LinkedLists, PriorityQueues, Breadth-first search
+		
     }
 }
