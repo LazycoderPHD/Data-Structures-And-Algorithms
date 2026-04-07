925c00b51d20aa33428a6b0c26acc8d501ae239a
LazycoderPHD <phatdao2309@gmail.com>
2026-04-08 02:19:44 +0700

    Added Dynamic-Array for List

---
+++ Dynamic-Array.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data_Structures_And_Algorithms
+{
+    internal class Dynamic_Array
+    {
+    }
+}
--- List.cs
+++ List.cs
@@ -3,6 +3,8 @@ public class ListDemo
 {
     public static void RunListDemo()
     {
-
+        int size;
+        int capacity = 4;
+        object[] a;
     }
 }
